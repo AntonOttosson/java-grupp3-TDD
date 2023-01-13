e44913b91a40e35dc808bf25b08f8da686c3850d
gurraoberg <oberg.gustav96@gmail.com>
2023-01-13 17:25:34 +0100

    Added some more tests

--- src/test/java/se/nackademin/InventoryTest.java
+++ src/test/java/se/nackademin/InventoryTest.java
@@ -2,9 +2,17 @@ package se.nackademin;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+/* import org.hamcrest.collection.IsMapContaining;
+
+import java.util.HashMap;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat; */
 
 public class InventoryTest {
-    
+
     @Test
     public void checkStockFalse() {
         Inventory inventory = new Inventory();
@@ -21,9 +29,30 @@ public class InventoryTest {
         assertTrue(check);
     }
 
-/*     @Test
+    @Test
     public void shouldAddToStock() {
         Inventory inventory = new Inventory();
-        assertThat(inventory.addToStock("Beanie", 100), IsMapContaining.hasEntry("Beanie", 100));
-    } */
+        inventory.addToStock("Keps", 53);
+        assertEquals("{Keps=53}", inventory.productMap.toString());
+    }
+
+    @Test
+    public void shouldSetNewProductName() {
+        Inventory inventory = new Inventory();
+        assertEquals(inventory.setProductName("Hatt 2"), inventory.productName);
+    }
+
+    @Test
+    public void shouldGetProductName() {
+        Inventory inventory = new Inventory();
+        inventory.setProductName("Basker");
+        String check = inventory.getProductName();
+        assertEquals("Basker", check);
+    }
+
+    @Test
+    public void shouldReturnNullOnGetProductName() {
+        Inventory inventory = new Inventory();
+        assertNull(inventory.getProductName());
+    }
 }
